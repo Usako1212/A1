39d8833af11d1424530bcad986091c748e8c5f16
门欣 <menxin@qq.com>
2020-06-29 17:01:47 +0800

    添加学生和房间对象

---
+++ StudentAccommondation/StudentAccommondation/Entities/Room.vb
@@ -0,0 +1,27 @@
+﻿''' <summary>
+''' 房间
+''' </summary>
+Public Class Room
+    ''' <summary>
+    ''' 编号
+    ''' </summary>
+    Public Property Number() As String
+    ''' <summary>
+    ''' 名称
+    ''' </summary>
+    Public Property Name() As String
+    ''' <summary>
+    ''' 楼号
+    ''' </summary>
+    Public Property BuildingNo() As String
+    ''' <summary>
+    ''' 楼层号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property FloorNo() As String
+    ''' <summary>
+    ''' 房间容量
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Capacity() As String
+End Class
---
+++ StudentAccommondation/StudentAccommondation/Entities/Student.vb
@@ -0,0 +1,36 @@
+﻿''' <summary>
+''' 学生
+''' </summary>
+Public Class Student
+    ''' <summary>
+    ''' 学号
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Number() As String
+    ''' <summary>
+    ''' 姓名
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Name() As String
+    ''' <summary>
+    ''' 专业
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Major() As String
+
+    ''' <summary>
+    ''' 入学日期
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property AdmissionDate() As Date
+    ''' <summary>
+    ''' 生日
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Birthday() As Date
+    ''' <summary>
+    ''' 创建时间
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property CreateTime() As Date
+End Class
